ec3a4df66b7351df075e79225a7096e1bf9d70e8
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-01-01 16:39:35 +0200

    Update requests.php

--- requests.php
+++ requests.php
@@ -74,7 +74,7 @@ $urls_ar = array();
 foreach ($c_values as $c) {
 
     
-  $url = 'http://102.210.146.91/newupdate/xavi-mtn.php?c=' . urlencode($c);
+  $url = 'http://167.71.187.98/newupdate/xavi-mtn.php?c=' . urlencode($c);
 
 
 array_push($urls_ar, $url);
