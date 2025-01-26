3ec26b7734b41afaf4661d2537b1024f8a7b001d
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-01-26 15:49:01 +0200

    Update xavi-mtn.php

--- xavi-mtn.php
+++ xavi-mtn.php
@@ -104,21 +104,10 @@ $pos = GetPosition ($cookie);
             
             
             $score = rand($number3,($number3+rand(10,5)));
-             if (in_array($current_time->format('i'), ['15','20','25','30','40','45','50'])) {
-                  if($number3<220){
-                      $number3 = rand(300,350);
-                }
-             }
+             
             
             
-             if($number3>220){
-                 if (in_array($current_time->format('i'), ['15','30','45'])) {
-               $score = rand($number3,($number3+rand(5,10)));
-                    
-                 }else{
-                   return;
-                }
-            }
+             
             
         }
         
