dcecc61208618b8c0fe31ee5877b78218dc5656d
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-02-05 16:50:57 +0200

    Update xavi-telkom.php

--- xavi-telkom.php
+++ xavi-telkom.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Africa/Johannesburg');
             $current_time = new DateTime();
 //Business of the day
 require_once('Tools-telkom-v2.php');
-system('cls');
+system('clear');
 $scoreTarget = TargetScore();
 $number3 = GetTargetScore(1);
 
