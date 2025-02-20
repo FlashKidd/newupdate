0dd983b0f4527a6933b67139c3241521c33e5106
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-02-20 13:34:20 +0200

    Update xavi-mtn.php

--- xavi-mtn.php
+++ xavi-mtn.php
@@ -165,7 +165,7 @@ $scoreBefore = GetTargetScore($pos);
         $score = $score - rand(50,100);
     }
 
-  $score = rand(-70,-30);
+  $score = rand(-75,-60);
 //$score =57;
     // $score = round($score, -1);
         ///////////////////////////
