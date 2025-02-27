49614d7422f410a72398a375c874f8e0ffba7e3e
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-02-27 20:19:56 +0200

    Update Tools-mtn-v2.php

--- Tools-mtn-v2.php
+++ Tools-mtn-v2.php
@@ -341,8 +341,8 @@ curl_close($ch);
 
 
 function generateRandomDivisionData($number,$url,$power,$memory,$increment,$uA) {
-$min = 10;
- $max = 50;
+$min = 1000;
+ $max = 3000;
     
    $data = [];
     // Generate a random number between 200 and 600
