ff4a76b35b571c3527b795e99acf94b7d1cdd3be
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-02-28 10:39:35 +0200

    Update Tools.php

--- Tools.php
+++ Tools.php
@@ -342,7 +342,7 @@ curl_close($ch);
 
 function generateRandomDivisionData($number,$url,$power,$memory,$increment,$uA) {
  $min = 1;
- $max = 25;
+ $max = 17;
     
    $data = [];
     // Generate a random number between 200 and 600
