4337231d1d13877da94be5eaf8555aaa68d718ef
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-03-14 13:56:12 +0200

    Update xavi-test.php

--- xavi-test.php
+++ xavi-test.php
@@ -109,10 +109,10 @@ $scoreBefore = GetTargetScore($pos);
            $testSom = GetTargetScore($pos);
             
             
-             $score = $number3+rand(1,50);
+             $score = $number3+rand(1,100);
             
              if($number3-$testSom>100){
-                $score = $testSom+rand(1,50);
+                $score = $testSom+rand(1,100);
          }
             
            
