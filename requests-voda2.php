06b902914a8d7f52096d683a9a92dcff2ad7a05c
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-03-15 16:55:35 +0200

    Update requests-voda2.php

--- requests-voda2.php
+++ requests-voda2.php
@@ -41,7 +41,7 @@ $c_values =[
 
 $urls_ar = array();
 shuffle($c_values); 
-$randomItems = array_slice($c_values, 0, 3); 
+$randomItems = array_slice($c_values, 0, rand(1,3)); 
 
 foreach ($randomItems as $c) {
 
