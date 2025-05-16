71e183f342e28adfd546ddb3f7ce7e5415ee4e0f
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-05-16 13:19:15 +0200

    Update requests.php

--- requests.php
+++ requests.php
@@ -35,8 +35,7 @@ $c_values = [
 $urls_ar = array();
 shuffle($c_values); 
 $randomItems = array_slice($c_values, 0, 3); 
-$serverIP = $_SERVER['SERVER_ADDR'];
-
+$serverIP = gethostbyname(gethostname());
 echo "\nIP ADDR: $serverIP";
 foreach ($randomItems as $c) {
 
