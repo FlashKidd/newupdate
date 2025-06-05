40db644d388af42fb4ebe2b8e17b8dbc8e31e90b
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-06-05 20:07:34 +0200

    Update xavi.php

--- xavi.php
+++ xavi.php
@@ -149,7 +149,7 @@ if ($pos <= 3 || $pos == 0) {
 
     echo "\n Our score and range $min - $max := $score";
 
-    while ($score > $testSom) {
+    while ($score < $testSom) {
        $score += rand(1,10);
       }
    
