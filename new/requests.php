bed5ffb6b91fa2423d7077e0a2e0400fa87f4ee8
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-08-14 18:55:12 +0200

    Update requests.php

--- new/requests.php
+++ new/requests.php
@@ -44,7 +44,7 @@ $serverIP = trim(gethostbyname(gethostname()));
 echo "\nIP ADDR: $serverIP";
 $urls = [];
 foreach ($urls_ar as $c) {
-    $urls[] = 'http://' . $serverIP . '/newupdate/xavi-test.php?c=' . urlencode($c);
+    $urls[] = 'http://' . $serverIP . '/newupdate/xavi-mtn.php?c=' . urlencode($c);
 }
 
 $curl->get($urls, function($result) {
