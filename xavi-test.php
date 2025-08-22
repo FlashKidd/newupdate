31f1e3bb4611c582715a87880e2311b563ef1fcc
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-08-22 13:49:19 +0200

    Update xavi-test.php

--- xavi-test.php
+++ xavi-test.php
@@ -137,7 +137,7 @@ $success = false;
 $currentScore = null;
 
 // Build and shuffle the score list so each score is attempted once in random order
-$max = 400;
+$max = 300;
 $count = 10;
 
 $min = $max - ($count - 1); 
