a321371c4fb4de5cce0ee13126c556432f23ed8b
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-08-27 23:22:52 +0200

    Update Tools-mtn-v2.php

--- Tools-mtn-v2.php
+++ Tools-mtn-v2.php
@@ -336,7 +336,7 @@ function generateRandomDivisionData($number,$url,$power,$memory,$increment,$uA) 
 //  $max = 100;
     
  $min = 10;
- $max = 100;
+ $max = 500;
     
   if ($number <= $max) {
     // If number is $max or less, output only 0.
