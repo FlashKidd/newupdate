ff036855f4652a401609eaa8b23e4fc21230c41e
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-10-18 15:45:33 +0200

    Update max and step values for score calculation

--- xavi-telkom.php
+++ xavi-telkom.php
@@ -145,9 +145,9 @@ $currentScore = null;
 // shuffle($scores);
 
 
-$max = 1200;
+$max = 4900;
 $count = 10;
-$step = 10;
+$step = 100;
 
 // calculate minimum based on max, step, and count
 $min = $max - ($count - 1) * $step;
