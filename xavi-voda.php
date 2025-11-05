baf3c1636eaf11778cf75216d80bb317e6a36ca5
FlashKidd <55650312+FlashKidd@users.noreply.github.com>
2025-11-06 01:13:55 +0200

    Change max score to a fixed value of 37956
    Set a fixed maximum score value for the game.

--- xavi-voda.php
+++ xavi-voda.php
@@ -149,7 +149,7 @@ $success = false;
 $currentScore = null;
 
 // Build and shuffle the score list so each score is attempted once in random order
-$max = rand(37000,37950);
+$max = 37956;
 $count = 10;
 
 $min = $max - ($count - 1); 
